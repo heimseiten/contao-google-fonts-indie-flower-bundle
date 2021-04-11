770fa96eb2dd98ee5a178a612c019c997239d824
Niels <info@heimseiten.de>
2021-04-11 15:25:58 +0200

    faster site loading

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,5 +1,5 @@
 <?php
 
 if (TL_MODE == 'FE') {
-    $GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaogooglefontsindieflower/css/contao-google-fonts-indie-flower-bundle.css|static';
+    $GLOBALS['TL_HEAD'][] = '<link rel="preload" href="/bundles/heimseitencontaogooglefontsindieflower/css/contao-google-fonts-indie-flower-bundle.css" as="style" onload="this.onload=null; this.rel=\'stylesheet\';">';
 }
